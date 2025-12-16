27dd3c8c1e878f12bef151d8757d984ee6c86a4e
Mudassar Ahmad <mlkmud56@gmail.com>
2025-12-16 09:18:59 +0500

    change hardcoded urls

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api";
+const API_URL = process.env.VITE_API_URL;
 
 export const registerUser = (data) =>
   axios.post(`${API_URL}/auth/register`, data);
